dd081525dd9a5e251225d1a7a6efd3c774735540
Ekaji <ekajionah@gmail.com>
2022-11-22 12:02:10 +0100

    update

--- components/homepageComponents/Activities.jsx
+++ components/homepageComponents/Activities.jsx
@@ -9,9 +9,9 @@ export default function Activities() {
         <div className='text-center'>
             <h3 className='text-gold font-mavin mt-24 text-2xl pt-8'  >Our Activities</h3>
             <h2 className='font-mavin text-3xl my-3 '>Our Learning Offers</h2>
-            <p className='text-greyBlack font-["openSans"] '> Learning would become fun because it would bubble up from within <br className='hidden lg:block' /> and not be forced in from without.</p>
+            <p className='text-greyBlack font-["openSans"]'> Learning would become fun because it would bubble up from within <br className='hidden lg:block' /> and not be forced in from without.</p>
         </div>
-        <div className='lg:hidden'>
+        <div className='lg:hidden '>
           <ConditionalSlider /> 
         </div>
         <div className='hidden lg:block'>
