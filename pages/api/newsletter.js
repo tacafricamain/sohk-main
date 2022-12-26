f774e58699c43c9a6f5be8de3dc8033751f96923
Ekaji <ekajionah@gmail.com>
2022-12-27 00:50:35 +0100

    update

---
+++ pages/api/newsletter.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        title: 'TYB Junior Championship',
+        description: 'The school of hard knocks marks its presence at the TYB junior Golf championship',
+        link: 'https://www.dropbox.com/s/qfus7g6x6hqj8i3/TYB%20Junior%20Championship.pdf?dl=1'
+    },
+    {
+        title: 'Junior golf championship',
+        description: 'Leaderjoe1808 foundation if a registered non profit organization founded in 2016 and registered on the 11 of April 2019',
+        link: 'https://www.dropbox.com/s/qz3gdk2j0p7gkwc/Junior%20%20Golf%20Championship%20.pdf?dl=0'
+    },
+    {
+        title: 'SOHK 2nd Edition NSL',
+        description: 'SOHK 2nd Edition NSL',
+        link: 'https://www.dropbox.com/s/votlzw27b0u03fk/SOHK 2nd Edition NSL.pdf?dl=1'
+    },
+    {
+        title: 'Kids Drone Academy Volume 1',
+        description: 'Kids Drone Academy Volume 1',
+        link: 'Kids-Drone-Academy-Volume-1.a7616a5e77c94c7a9561e09af60121c8'
+    },
+    {
+        title: 'IBB golf club volume 4',
+        description: 'IBB golf club volume 4',
+        link: 'https://www.dropbox.com/s/n34tjewi79m9mjs/IBB%20GOLF%20CLUB%20Volume%204.pdf?dl=0'
+    },
+    {
+        title: 'go green go global',
+        description: 'Welcome to the first edition of go-green go-global project, an initiative of TAC Africa, powered by the School of Hardknocks',
+        link: 'https://www.dropbox.com/s/rcw8qxlfar0ss9q/Go%20green%21%20Go%20Global%21.pdf?dl=0'
+    },
+    // {
+    //     title: '',
+    //     description: '',
+    //     link: ''
+    // },
+]
+
--- pages/newsletters/index.jsx
+++ pages/newsletters/index.jsx
@@ -1,51 +1,42 @@
 /* eslint-disable react/jsx-key */
-// import { GetStaticProps } from 'next';
+// import {GetStaticProps} from 'next';
 import React from 'react'
-import { getDatabase } from '../../lib/notion';
-import { Text } from './[id]';
-import Link from 'next/link';
+import { data } from '../api/newsletter';
 
-const databaseId = process.env.NOTION_BLOG_DATABASE_ID || ""
-
-export const getStaticProps = async () => {
-  const newsletter = await getDatabase(databaseId, 'newsletter');
-
-  return {
-    props: {
-      posts: newsletter,
-    }
-  };
-};
-
-export default function Newsletters({ posts } ) {
-    console.log(posts[0].properties.excerpt.rich_text, '<>' )
-  return (
-    <div className=' bg-white '>
-        <div className="pt-32 pb-32 flex flex-col gap-4 items-center justify-center">
-            {
-            posts.map((post , i ) => (
-                <Link href={`${post.properties.externalURL.url}`}  >
-                  <a className="rounded-sm w-10/12 lg:w-1/2 grid grid-cols-12  bg-gray-200 shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform">
-                    {/* <!-- Title --> */}
-                    <div className="col-span-11 xl:ml-5">
-                    <p className="text-blue-600 font-semibold"> 
-                        <Text text={post.properties.Name.title} />
-                    </p>
-                    </div>
+export default function Newsletters() {
+    return (
+        <div className=' bg-white '>
+            <div className="pt-32 pb-32 px-6 flex flex-col gap-4 items-center justify-center">
+                {
+                data.map((post, i) => (
+                    <a className='w-full lg:w-[600px] mx-auto' href={
+                        `${
+                            post.link
+                    }`
                     
-                    {/* <!-- Description --> */}
+                    }>
+                        <div className="rounded-sm  grid grid-cols-12  bg-gray-200 shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform">
+                            {/* <!-- Title --> */}
+                            <div className="col-span-11 xl:ml-5">
+                                <p className="text-blue-600 font-semibold">
+                                       { post.title}
+                                </p>
+                            </div>
 
-                    <div className=" col-span-11 xl:ml-5">
-                        <p className="text-sm text-gray-800 font-light "> 
-                            <Text text={post.properties.excerpt.rich_text} />
-                        </p>
-                    </div>
-                  </a>
-                    
-                </Link >
-            ))
-            }
+                            {/* <!-- Description --> */}
+
+                            <div className=" col-span-11 xl:ml-5">
+                                <p className="text-sm text-gray-800 font-light ">
+                                    {
+                                        post.description
+                                    }
+                                </p>
+                            </div>
+                        </div>
+
+                    </a>
+                ))
+            } </div>
         </div>
-    </div>
-  )
+    )
 }
