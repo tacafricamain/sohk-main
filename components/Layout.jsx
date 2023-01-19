0953f77faa05998f7c4fcb273f387df5ed4eb604
Ekaji <ekajionah@gmail.com>
2023-01-19 12:56:30 +0100

    update to favicon and og image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,22 +2,23 @@ import Footer from './Footer'
 import NavBar from './NavBar'
 import Membership from './homepageComponents/Membership'
 import Head from 'next/head'
+import og from '../public/og.jpg'
 
 
 export default function Layout({children}) {
     return(
         <>
             <Head>
-            <meta property="og:site_name" content="schoolofhardknocks" />
+                <meta property="og:site_name" content="schoolofhardknocks" key="ogsitename" />
                 {/* twitter */}
-                <meta name="twitter:card" content="/og.jpg" />
+                <meta name="twitter:card" content="/og.jpg" key="ogimage" />
                 <meta name="twitter:title" content="welcome to the website of the school of hard knocks Nigeria" />
                 <meta name="twitter:description" content="the school of hard knocks nigeria" />
-                <meta name="twitter:image" itemProp="image"  content="/og.jpg" />
+                <meta name="twitter:image" itemProp="image"  content="/og.jpg" key="ogimage" />
                 {/* web icon */}
                 <meta property="og:title" content="welcome to the website of the school of hard knocks Nigeria" />
                 <meta property="og:description" content="the school of hard knocks nigeria" />
-                <meta property="og:image" itemProp="image" content="/og.jpg" />
+                <meta property="og:image" itemProp="image" content="/og.jpg" key="ogimage" />
                 <meta property="og:updated_time" content="updatedtime" />
                 {/*  */}
                 <link itemProp="thumbnailUrl" href="/favicon.ico" /> 
