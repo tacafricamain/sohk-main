0354d40e7d6a5b013607aff2159bd3314b26f8cf
Ekaji <ekajionah@gmail.com>
2023-01-19 11:32:54 +0100

    update to favicon and og image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,14 +9,14 @@ export default function Layout({children}) {
         <>
              <Head>
         {/* twiter */}
-                <meta name="twitter:card" content="/aboutushero.png" />
+                <meta name="twitter:card" content="/og.jpg" />
                 <meta name="twitter:title" content="welcome to the website of the school of hard knocks Nigeria" />
                 <meta name="twitter:description" content="the school of hard knocks nigeria" />
-                <meta name="twitter:image" content="/aboutushero.png" />
+                <meta name="twitter:image" content="/og.jpg" />
                 {/* web icon */}
                 <meta property="og:title" content="welcome to the website of the school of hard knocks Nigeria" />
                 <meta property="og:description" content="the school of hard knocks nigeria" />
-                <meta property="og:image" content="/aboutushero.png" />
+                <meta property="og:image" content="/og.jpg" />
                 
                 <link rel="shortcut icon" href="/favicon.ico" />
             </Head>
