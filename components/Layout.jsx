aa1988db2124a6665161deb226cf5c8dce0d291d
Ekaji <ekajionah@gmail.com>
2023-01-19 14:50:51 +0100

    update to favicon and og image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,24 +8,7 @@ import og from '../public/og.jpg'
 export default function Layout({children}) {
     return(
         <>
-            <Head>
-                <meta property="og:site_name" content="schoolofhardknocks" key="ogsitename" />
-                {/* twitter */}
-                <meta name="twitter:card" content="/og.jpg" key="ogimage" />
-                <meta name="twitter:title" content="welcome to the website of the school of hard knocks Nigeria" />
-                <meta name="twitter:description" content="the school of hard knocks nigeria" />
-                <meta name="twitter:image" itemProp="image"  content="/og.jpg" key="ogimage" />
-                {/* web icon */}
-                <meta property="og:title" content="welcome to the website of the school of hard knocks Nigeria" />
-                <meta property="og:description" content="the school of hard knocks nigeria" />
-                <meta property="og:image" itemProp="image" content="/og.jpg" key="ogimage" />
-                <meta property="og:updated_time" content="updatedtime" />
-                {/*  */}
-                <link itemProp="thumbnailUrl" href="/favicon.ico" /> 
-                <meta property="og:image:type" content="image/jpeg" />
-                
-                <link rel="shortcut icon" href="/favicon.ico" />
-            </Head>
+ 
             <NavBar />
                 { children }
             <Membership />
--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,24 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      
+        <Head>
+            <meta property="og:site_name" content="schoolofhardknocks" key="ogsitename" />
+            {/* twitter */}
+            <meta name="twitter:card" content="https://res.cloudinary.com/tacafrica/image/upload/v1674135945/my_folder/Website%20pics/og_ejtymf.jpg" key="ogimage" />
+            <meta name="twitter:title" content="welcome to the website of the school of hard knocks Nigeria" />
+            <meta name="twitter:description" content="the school of hard knocks nigeria" />
+            <meta name="twitter:image" itemProp="image"  content="https://res.cloudinary.com/tacafrica/image/upload/v1674135945/my_folder/Website%20pics/og_ejtymf.jpg" key="ogimage" />
+            {/* web icon */}
+            <meta property="og:title" content="welcome to the website of the school of hard knocks Nigeria" />
+            <meta property="og:description" content="the school of hard knocks nigeria" />
+            <meta property="og:image" itemProp="image" content="https://res.cloudinary.com/tacafrica/image/upload/v1674135945/my_folder/Website%20pics/og_ejtymf.jpg" key="ogimage" />
+            <meta property="og:updated_time" content="updatedtime" />
+            {/*  */}
+            <link itemProp="thumbnailUrl" href="/favicon.ico" /> 
+            <meta property="og:image:type" content="image/jpeg" />
+            
+            <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
