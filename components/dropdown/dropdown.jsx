dd21af3538c231ec6f10221ab2b2f7476aedc3de
Ekaji <ekajionah@gmail.com>
2023-03-13 12:29:43 +0100

    update to dropdown hover color

--- components/dropdown/dropdown.jsx
+++ components/dropdown/dropdown.jsx
@@ -22,7 +22,7 @@ const DropDownButton = ({
 
       <button
         id="dropdownButton"
-        className="capitalize hover:bg-blue-300 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm 2xl:text-lg px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="capitalize hover:bg-gold focus:ring-4 focus:outline-none font-medium rounded-lg text-sm 2xl:text-lg px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
         {label}{' '}
