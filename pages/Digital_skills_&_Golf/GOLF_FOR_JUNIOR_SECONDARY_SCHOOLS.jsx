d7b4fa12f0c30827825166aeebcc983c97aba706
Ekaji <ekajionah@gmail.com>
2023-03-14 13:24:12 +0100

    update

--- pages/Digital_skills_&_Golf/GOLF_FOR_JUNIOR_SECONDARY_SCHOOLS.jsx
+++ pages/Digital_skills_&_Golf/GOLF_FOR_JUNIOR_SECONDARY_SCHOOLS.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 
 import React from 'react'
 
@@ -14,12 +15,53 @@ export default function GOLF_FOR_JUNIOR_SECONDARY_SCHOOLS () {
               </span> */}
             </div>
             <div className="w-full aspect-video bg-gray-400 object-contain rounded-xl overflow-hidden">
-                <img src="https://res.cloudinary.com/tacafrica/image/upload/v1675094158/my_folder/IBB%20golf%20tour/YOD-20230113065_ejac0u.jpg" alt="project 774" />
+                <img src="/golf/FB_IMG_1678792613488.jpg" alt="project 774" className='w-full' />
             </div>
             <div className="flex flex-col gap-4 w-10/12 max-md:w-full mx-auto py-12 justify-between">
               <div className=" space-y-4">
                 <p className="text-base  text-gray-800 leading-6">
                 Golf fosters friendly association between players of all ages and abilities. The Junior Secondary Golf Programme is designed to introduce students to the main skills of Golf through activities that can be modified for their abilities. The Golf Curriculum in the Junior Secondary School Category gives a solid foundation of the rules, regulations and etiquette of the game.  This initiative will allow children in the Junior Government Secondary Schools to have a quality Golf experience at school and instill in them the confidence to explore the sport as a hobby or career, either at this moment or in the future.
+                </p>
+              <div className='flex justify-center my-4 '>
+                  <div className='container h-full grid lg:grid-cols-4 lg:grid-rows-2 lg:gap-4 lg:h-[30rem] '>
+                        <div className='lg:col-span-2 lg:row-span-2 bg-sky-400'>
+                              <img 
+                                  src="/golf/FB_IMG_1678793135679.jpg" 
+                                  alt="golf for schools"
+                                  className='max-h-full w-full object-cover'
+                                  />
+                      </div>
+                        <div className='bg-red-300 '>
+                        <img 
+                          src="/golf/FB_IMG_1678792440010.jpg" 
+                          alt="golf for schools"
+                          className='max-h-full w-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-green-500">
+                      <img 
+                          src="/golf/FB_IMG_1678792431402.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-blue-400">
+                      <img 
+                          src="/golf/FB_IMG_1678793082504.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-yellow-400">
+                      <img 
+                          src="/golf/FB_IMG_1678793071600.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>    
+                  </div>
+                </div>
+                <p>
                 And because Golf is both fun and challenging, with a low risk of injury, it is a very safe sport to play. Participants in this category will be introduced to a more comprehensive introduction to the basics of Golf course, and be exposed to exciting Golf related opportunities as well as learn valuable life skills such as Leadership, Communication, Self-management, Sportsmanship, Perseverance etc. They would be able to enjoy access to more equipment, coaching and mentorship resources as well as training from professionals. 
                 </p>
               </div>
--- pages/Digital_skills_&_Golf/GOLF_FOR_PRIMARY_SCHOOLS.jsx
+++ pages/Digital_skills_&_Golf/GOLF_FOR_PRIMARY_SCHOOLS.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 export default function GOLF_FOR_PRIMARY_SCHOOLS() {
@@ -12,13 +13,54 @@ export default function GOLF_FOR_PRIMARY_SCHOOLS() {
             Technology Against Crime | March 7, 2023
             </span> */}
           </div>
-          <div className="w-full aspect-video bg-gray-400 object-contain rounded-xl overflow-hidden">
-              <img src="https://res.cloudinary.com/tacafrica/image/upload/v1675094173/my_folder/IBB%20golf%20tour/YOD-20230113080_porgid.jpg" alt="project 774" />
+          <div className="w-full aspect-video bg-gray-400 object-c ontain rounded-xl overflow-hidden">
+              <img src="/golf/FB_IMG_1678793117522.jpg" className='w-full object-fill' alt="project 774" />
           </div>
           <div className="flex flex-col gap-4 w-10/12 max-md:w-full mx-auto py-12 justify-between">
             <div className=" space-y-4">
               <p className="text-base  text-gray-800 leading-6">
               The School of HardKnocks Foundation is launching a new programme where we offer children in primary schools their first introduction to Golf training. We strongly believe that these kids will enjoy playing golf and that the game will help them build qualities and life skills that they would need as they grow older. The age of these children was put into consideration when preparing the resources of the Golf Curriculum for primary schools, so that they can find the game engaging and interesting. We have a lot of testimonies from young children to attest to the fact that learning Golf at an early age has helped to enhance their learning capabilities and had a positive impact on their overall well-being.
+              </p>
+              <div className='flex justify-center my-4 '>
+                  <div className='container h-full grid lg:grid-cols-4 lg:grid-rows-2 lg:gap-4 lg:h-[30rem] '>
+                        <div className='lg:col-span-2 lg:row-span-2 bg-sky-400'>
+                              <img 
+                                  src="/golf/FB_IMG_1678793135679.jpg" 
+                                  alt="golf for schools"
+                                  className='max-h-full w-full object-cover'
+                                  />
+                      </div>
+                        <div className='bg-red-300 '>
+                        <img 
+                          src="/golf/FB_IMG_1678792440010.jpg" 
+                          alt="golf for schools"
+                          className='max-h-full w-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-green-500">
+                      <img 
+                          src="/golf/FB_IMG_1678792431402.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-blue-400">
+                      <img 
+                          src="/golf/FB_IMG_1678793082504.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>
+                      <div className="bg-yellow-400">
+                      <img 
+                          src="/golf/FB_IMG_1678793071600.jpg" 
+                          alt="golf for schools"
+                          className='max- h-full object-cover'
+                          />
+                      </div>    
+                  </div>
+                </div>
+                <p>
               Golf is a game of life. Whether your dream is to make it a hobby or a career, learning the basics of Golf at an early age will serve you forever. The Primary School Golf Programme will no doubt provide mentoring and coaching opportunities to these young ones, which will help shape their future. The wider benefits of Golf can help the physical and mental wellbeing of more young people. Over the next few years, more primary schools will be included in the programme because we believe that there are a lot of children who will be excited to explore the game of Golf and its tremendous benefits.
               </p>
             </div>
