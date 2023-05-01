edcf296e01625847483bc4b23838de1c2f2c02f2
Ekaji <ekajionah@gmail.com>
2023-05-01 18:28:30 +0100

    update to gallery

---
+++ config/cloudinary/galleryImages/opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club.js
@@ -0,0 +1,126 @@
+// TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club.
+export const TAC_Africa_School_Of_Hardknocks_Nigeria_Corporate_United_Football_Club_Co_Host_the_Maiden_Novelty_Football_to_officially_Mark_the_opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club = {
+    title: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club.',
+    slug: 'TAC_Africa_School_Of_Hardknocks_Nigeria_Corporate_United_Football_Club_Co-Host_the_Maiden_Novelty_Football_to_officially_Mark_the_opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club',
+    content: "TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club.",
+    date: '2022',
+    displayImage: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949116/sport_saisia/IMG_1177_muiw1n.jpg',
+    images: [
+        {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949116/sport_saisia/IMG_1177_muiw1n.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },
+        {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682950379/sport_saisia/IMG_1169_cdoxeu.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },
+        {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949116/sport_saisia/IMG_1177_muiw1n.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949115/sport_saisia/IMG_9998_f2cduq.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949112/sport_saisia/IMG_9996_n1gamq.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949109/sport_saisia/IMG_9988_nsf4vb.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949089/sport_saisia/IMG_9920_diiepg.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949079/sport_saisia/IMG_9911_wi5vkt.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949078/sport_saisia/IMG_9897_yeus3e.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949076/sport_saisia/IMG_9900_r6vuyv.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949074/sport_saisia/IMG_9896_s7cuux.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949066/sport_saisia/IMG_9886_lawmut.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949066/sport_saisia/IMG_9889_vkrfnw.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949063/sport_saisia/IMG_9888_lpg7zu.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949062/sport_saisia/IMG_1174_ylfcmt.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949040/sport_saisia/IMG_1188_yjm93e.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949033/sport_saisia/IMG_1185_bia9os.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949032/sport_saisia/IMG_1182_mjuwov.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949019/sport_saisia/IMG_1180_hypmvw.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949016/sport_saisia/IMG_1176_mdrv9z.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949013/sport_saisia/IMG_1179_igh3b9.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949011/sport_saisia/IMG_1171_e6ijxs.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949009/sport_saisia/IMG_1175_rcv3wp.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682949002/sport_saisia/IMG_1173_kuidkx.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948981/sport_saisia/IMG_1168_dp7c8h.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948940/sport_saisia/IMG_1166_qnjskq.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948933/sport_saisia/IMG_1163_gc9lxc.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948932/sport_saisia/IMG_1165_zq8eti.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948908/sport_saisia/IMG_1162_ugs2fg.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948880/sport_saisia/IMG_1155_t6yvh4.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948878/sport_saisia/IMG_1149_khn7xo.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },   {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948877/sport_saisia/IMG_1151_jts4ut.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },
+        {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948870/sport_saisia/IMG_1140_v3eute.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        }, {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948866/sport_saisia/IMG_1136_lmgkeu.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        }, {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948834/sport_saisia/IMG_1087_mczqqy.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        }, {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948834/sport_saisia/IMG_1086_ejtcos.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        }, {
+            src: 'https://res.cloudinary.com/tacafrica/image/upload/v1682948826/sport_saisia/IMG_1075_lndkhu.jpg',
+            alt: 'TAC Africa, School Of Hardknocks Nigeria, Corporate United Football Club Co-Host the Maiden Novelty Football to officially Mark the opening of Corporate United Football Pitch and to Honour the Birthday of Mr. Goodwin Ekpa, the President of Corporate United Football Club'
+        },
+    ]
+
+}
--- pages/api/galleryImages.js
+++ pages/api/galleryImages.js
@@ -10,8 +10,12 @@ import {Vitality_Clubhouse_Tour} from "../../config/cloudinary/galleryImages/Vit
 import {TYB_Golf_Tour} from "../../config/cloudinary/galleryImages/TYB_Golf_Tour";
 import { Certification_Of_Endorsement_To_Train_Secondary_Schools_Students } from "../../config/cloudinary/galleryImages/Certification_Of_Endorsement_To_Train_Secondary_Schools_Students"
 import { UN_International_Day_of_ZERO_WASTE } from "../../config/cloudinary/galleryImages/UN_International_Day_of_ZERO_WASTE";
+import { TAC_Africa_School_Of_Hardknocks_Nigeria_Corporate_United_Football_Club_Co_Host_the_Maiden_Novelty_Football_to_officially_Mark_the_opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club } from "../../config/cloudinary/galleryImages/opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club"
 
 export const images = [
+    {
+        ...TAC_Africa_School_Of_Hardknocks_Nigeria_Corporate_United_Football_Club_Co_Host_the_Maiden_Novelty_Football_to_officially_Mark_the_opening_of_Corporate_United_Football_Pitch_and_to_Honour_the_Birthday_of_Mr_Goodwin_Ekpa_the_President_of_Corporate_United_Football_Club
+    },
     {
         ...UN_International_Day_of_ZERO_WASTE
     },
