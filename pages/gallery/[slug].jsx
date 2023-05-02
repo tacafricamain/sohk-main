dab0f05fd813f47befb6e8f2df0d27ba7181d49e
Ekaji <ekajionah@gmail.com>
2023-05-02 12:41:29 +0100

    update to drone safety day

--- pages/gallery/[slug].jsx
+++ pages/gallery/[slug].jsx
@@ -1,20 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { images } from '../api/galleryImages';
+import Head from 'next/head';
 
 const ImageGallery = ({ post }) => {
   return (
-    <div className="container px-5 py-24 mx-auto">
-      <div className="flex flex-col w-full mb-20">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{post[0]?.title}</h1>
-        <p className="mt-4 max-w-xl text-gray-500 lg:text-2xl sm:text-xl sm:leading-relaxed">{post[0]?.content} </p>
-      </div>
-        <div key={5} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8 " >
-          {post[0]?.images.map(({ src, alt }, i) => (
-            <img key={i} src={src} alt={ alt} className="w-full h-[200px] object-cover transition ease-in-out delay-150 hover:scale-[2] " />
-          ))}
+    <>
+      <Head>
+      <title>{post[0]?.title}</title>
+      <meta name="description" content={images[0].src} />
+      </Head>
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{post[0]?.title}</h1>
+          <p className="mt-4 max-w-xl text-gray-500 lg:text-2xl sm:text-xl sm:leading-relaxed">{post[0]?.content} </p>
         </div>
-      </div>
+          <div key={5} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8 " >
+            {post[0]?.images.map(({ src, alt }, i) => (
+              <img key={i} src={src} alt={ alt} className="w-full h-[200px] object-cover transition ease-in-out delay-150 hover:scale-[2] " />
+            ))}
+          </div>
+        </div>
+    </>
   );
 }
 
