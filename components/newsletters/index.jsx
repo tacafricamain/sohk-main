359a944dbbb8f1a3d617032fc57c9399f6f40259
Ekaji <ekajionah@gmail.com>
2023-06-14 14:08:11 +0100

    update to link

--- components/newsletters/index.jsx
+++ components/newsletters/index.jsx
@@ -34,9 +34,11 @@ export default function Newsletters() {
                                 <a className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600" href="#">
 
                                 <Link href={ post.link}>
-                                    <span className=''>
-                                        Read more
-                                    </span>
+                                    <a>
+                                        <span className=''>
+                                            Read more
+                                        </span>
+                                    </a>
                                 </Link>
                                 <ion-icon class="flex-none w-3 h-3 ml-3 fill-blue-600 group-active:fill-current md hydrated" name="arrow-forward-outline" role="img" aria-label="arrow forward outline"></ion-icon>
                                 </a>            </div>
