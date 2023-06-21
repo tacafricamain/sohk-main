34b277fec6b6015b70d8e4579afa44d521bdcf61
Ekaji <ekajionah@gmail.com>
2023-06-21 13:51:46 +0100

    update to popup

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       {/* <HeroSection /> */}
             <Hero />
             <NewsLetter />
-            <NoticePopup  />
+            {/* <NoticePopup  /> */}
             <Objective />
             <Activities />
             <Promotion />
@@ -39,7 +39,7 @@ export default function Home() {
             <Testimonials />
             <Contact />
             <Map />
-            <BottomPopUp />
+            {/* <BottomPopUp /> */}
           </main>
   )
 }
